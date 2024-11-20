c3dbda9aee3caec22a8f3a550a36a7d6dd71648d
Yaroslav Antipov <yaroslav.antipov357@gmail.com>
2024-11-20 17:53:31 +0100

    First Working Solution

--- server.js
+++ server.js
@@ -12,6 +12,23 @@ app.get('/healthz', (_req, res) => {
   res.send({ status: 'OK' });
 });
 
+app.post('/move', (_req,res) => {
+  const rand = Math.random() * 10;
+  
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200);
+
+  if (rand <= 2.5) {
+    return res.send({ move: 'M' })
+  } else if (rand > 2.5 && rand <= 5) {
+    return res.send({ move: 'R' })
+  } else if (rand > 5 && rand <= 7.5) {
+    return res.send({ move: 'L' })
+  } else {
+    return res.send({ move: 'F' })
+  }
+})
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
